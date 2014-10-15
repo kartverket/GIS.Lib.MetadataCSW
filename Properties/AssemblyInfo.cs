ca1fdc4b1a6696f0d1d22e8f8216912321e337bf
Henning Jensen <henning@arkitektum.no>
2014-10-15 08:22:10 +0200

    Version 1.5.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arkitektum AS")]
 [assembly: AssemblyProduct("Arkitektum.GIS.Lib.MetadataCSW")]
-[assembly: AssemblyCopyright("Copyright © Arkitektum AS 2013")]
+[assembly: AssemblyCopyright("Copyright © Arkitektum AS 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyVersion("1.5.0")]
